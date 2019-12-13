c516947a257251545b0e9dfd4b899f8185b67985
Jon Schneider <jon.schneider@gmail.com>
2019-12-13 15:07:49 -0500

    Day 13 part 2

--- day-13/day-13-part-2.ts
+++ day-13/day-13-part-2.ts
@@ -1,10 +1,11 @@
 import * as fs from 'fs';
 import { IntcodeComputer } from './IntcodeComputer';
 
-const computer: IntcodeComputer = new IntcodeComputer("day-13/input.txt");
-computer.runProgram();
+let paddleX: number;
+let ballX: number;
 
-let blockTileCount = 0;
+let joystickDirection: number = 0;
+let score: number;
 
 function processOutputs() {
   while (computer.outputQueue.length > 1) {
@@ -12,11 +13,41 @@ function processOutputs() {
     const y = computer.outputQueue.shift();
     const tileId = computer.outputQueue.shift();
 
-    if (tileId === 2) {
-      blockTileCount++;
+    if (x === -1 && y === 0) {
+      score = tileId; 
+      continue;
+    }
+
+    if (tileId === 3) {
+      paddleX = x;
+    }
+    else if (tileId === 4) {
+      ballX = x;
+    }
+
+    if (tileId === 3 || tileId === 4) {
+      if (ballX < paddleX) {
+        // Left
+        joystickDirection = -1;
+      }
+      else if (paddleX < ballX) {
+        // Right
+        joystickDirection = 1;
+      }
+      else {
+        // Neutral
+        joystickDirection = 0;
+      }
     }
   }
 }
+
+const computer: IntcodeComputer = new IntcodeComputer("day-13/input.txt");
+
+// Free play -- no quarters needed!
+computer.memory[0] = 2;
+
+computer.runProgram();
 
 while(true) {
   if (computer.terminated) {
@@ -27,11 +58,10 @@ while(true) {
 
   // If the computer stopped running without terminating, then the program
   // is awaiting an input.
-  throw "Unexpected input instruction";
-  //computer.inputQueue.push(n);
-  //computer.runProgram();
+  computer.inputQueue.push(joystickDirection);
+  computer.runProgram();
 }
 
 processOutputs();
 
-console.log(blockTileCount);
+console.log(score);
