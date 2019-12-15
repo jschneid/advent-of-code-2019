ebaa4e8c5da19e68ac20e6b6672849f7aac07b5b
Jon Schneider <jon.schneider@gmail.com>
2019-12-15 15:35:00 -0500

    Day 15 - Explore area, print map of area

--- day-15/day-15.ts
+++ day-15/day-15.ts
@@ -39,26 +39,37 @@ function getRelativePositionAtDirection(position: Position, direction: number): 
   switch(direction) {
     case 1: // North
       return new Position(position.x, position.y + 1);
-      break;
     case 2: // South 
       return new Position(position.x, position.y - 1);
-      break;
     case 3: // West
       return new Position(position.x - 1, position.y);
-      break;
     case 4: // East
       return new Position(position.x + 1, position.y);
-      break;
     default: 
       throw "Unexpected direction: " + direction;
   }
 }
 
-function exploreLocation() {
+function getReverseOfDirection(direction: number): number {
+  switch(direction) {
+    case 1: // North
+      return 2;
+    case 2: // South 
+      return 1;
+    case 3: // West
+      return 4;
+    case 4: // East
+      return 3;
+    default: 
+      throw "Unexpected direction: " + direction;
+  }
+}
+
+function exploreLocation(location: Location) {
   for (let direction = 1; direction <= 4; direction++) {
     // Skip this direction if it has already been mapped
     const newPosition: Position = getRelativePositionAtDirection(robotPosition, direction);
-    const targetLocation: Location = getLocationAt(newPosition);
+    let targetLocation: Location = getLocationAt(newPosition);
     if (targetLocation) {
       continue;
     }
@@ -68,22 +79,88 @@ function exploreLocation() {
     computer.runProgram();
     
     // Get the result (read the output)
-
+    const result: number = computer.outputQueue.shift();
 
     // If we succeeded, explore it
-    // Then move back to the original position
+    if (result === 1 || result === 2) {
+      robotPosition = newPosition;
 
-    // Else, mark that location as being a wall
-    
+      targetLocation = new Location(newPosition, true);
+      map.push(targetLocation);
+
+      if (result === 2) {
+        oxygenSystemLocation = targetLocation;
+      }
+
+      exploreLocation(targetLocation);
+      
+      // Then move back to the original position
+      const reverseDirection = getReverseOfDirection(direction);
+      const oldPosition = getRelativePositionAtDirection(robotPosition, reverseDirection);
+      computer.inputQueue.push(reverseDirection);
+      computer.runProgram();
+      robotPosition = oldPosition;
+      computer.outputQueue.shift();
+    }
+    else {
+      // Else, mark that location as being a wall
+      targetLocation = new Location(newPosition, false);
+      map.push(targetLocation);
+    }
   }
 }
 
+function debugPrintMap() {
+  let minX = Number.MAX_VALUE;
+  let maxX = Number.MIN_VALUE;
+  let minY = Number.MAX_VALUE;
+  let maxY = Number.MIN_VALUE;
+  for (const location of map) {
+    if (location.position.x < minX) {
+      minX = location.position.x;
+    }
+    if (location.position.x > maxX) {
+      maxX = location.position.x;
+    }
+    if (location.position.y < minY) {
+      minY = location.position.y;
+    }
+    if (location.position.y > maxY) {
+      maxY = location.position.y;
+    }
+  }
 
-
+  let printedMap = "";
+  for (let y = minY; y <= maxY; y++) {
+    for (let x = minX; x <= maxX; x++) {
+      const location = getLocationAt(new Position(x, y));
+      if (location) {
+        if (oxygenSystemLocation && location.position.x === oxygenSystemLocation.position.x && location.position.y === oxygenSystemLocation.position.y) {
+          printedMap += "O";
+        }
+        else if (location.position.x === 0 && location.position.y === 0) {
+          printedMap += "S";
+        }
+        else if (location.isOpen) { 
+          printedMap += ".";
+        }
+        else {
+          printedMap += "█";
+        }
+      }
+      else {
+        printedMap += " ";
+      }
+    }
+    printedMap += "\n";
+  }
+  console.log(printedMap);
+}
 
 // 1. Map the entire space
 map.push(startLocation);
-
+exploreLocation(startLocation);
+debugPrintMap();
 
 // 2. Find the shortest path from 0,0 to the oxygen room
 
